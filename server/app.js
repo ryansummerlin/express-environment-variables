b4dfe4f4f18448379166e94f9b5f05b705dcd04e
ryansummerlin <ryansummerlin777@gmail.com>
2023-05-14 19:15:10 -0700

    Configured all local variables required using both command line and .env

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+
+// require('dotenv').config();
+
 const app = express();
-// Your code here
 
 app.get('/', (req, res) => {
     res.send(process.env.SECRET_MESSAGE);
